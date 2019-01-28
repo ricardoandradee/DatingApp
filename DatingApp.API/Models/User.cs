fe1b40b3f574518a6e610df4174a7f0c37b54f67
unknown <ricardoo.developer@gmail.com>
2019-01-28 13:32:59 -0200

    End of Section 13

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -13,7 +13,7 @@ namespace DatingApp.API.Models
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
-        public DateTime Lastactive { get; set; }
+        public DateTime LastActive { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
